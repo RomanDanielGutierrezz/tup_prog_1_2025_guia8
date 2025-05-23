ecdb1005f0b6adcaa5269bd8df58c77120ea8f78
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-05-23 15:24:11 +0200

    Agrego clase guia 8

---
+++ Guia_8/EJ1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+
+namespace EJ1
+{
+    internal class Program
+    {
+        #region variables
+        static int contador = 0, acumulador = 0, maximo = 0, minimo = 1000;
+        static int CantIngreso, Valor, Promedio = 0;
+        #endregion
+        static void Main(string[] args)
+        {
+            
+            #region Menu
+            Console.WriteLine("1 - Procesar un solo numero");
+            Console.WriteLine("2 - Procesar varios numero");
+            Console.WriteLine("3 - Mostrar maximo y minimo");
+            Console.WriteLine("4 - Mostrar Promedio");
+            Console.WriteLine("5 - Mostrar Cantidad de numeros ingresados");
+            Console.WriteLine("6 - Reiniciar Variables");
+            int entrada = Convert.ToInt32(Console.ReadLine());
+            #endregion
+            switch (entrada)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Ingresar Valor");
+                        Valor = Convert.ToInt32(Console.ReadLine());
+                        acumulador += Valor;
+                        contador++;
+                    }
+                    break;
+                case 2: 
+                    {
+                        #region Solicitar cantidad datos
+                        Console.WriteLine("Ingresar cantidad de datos");
+                        CantIngreso = Convert.ToInt32(Console.ReadLine());
+                        #endregion
+                        #region Iteracion
+                        for (int i = 1; i <= CantIngreso; i++)
+                        {
+                            #region Solicitar y acumular
+                            Console.WriteLine("Ingresar Valor");
+                            Valor = Convert.ToInt32(Console.ReadLine());
+                            acumulador += Valor;
+                            #endregion
+                            #region Maximo y Minimo
+                            if (Valor > maximo)
+                                maximo = Valor;
+
+                            if (Valor < minimo)
+                                minimo = Valor;
+                            #endregion
+                            contador++;
+                        }
+                        #endregion
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine($"El valor maximo es {maximo}");
+                        Console.WriteLine($"El valor minimo es {minimo}");
+                    }
+                    break;
+                case 4:
+                    {
+                        #region Verificar
+                        if (maximo != 0)
+                        {
+                            #region Promedio
+                            Promedio = acumulador / contador;
+                            #endregion
+                        }
+                        #endregion
+                        Console.WriteLine($"El promedio es {Promedio}");
+                    }break;
+                case 5:
+                    {
+                        Console.WriteLine($"La cantidad de numero ingresados es {contador}");
+                    }break;
+                case 6:
+                    {
+                        contador = 0; acumulador = 0; maximo = 0; minimo = 1000;
+                        CantIngreso = 0; Valor = 0; Promedio = 0;
+                    }
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Numero no valido");
+                    }break;
+            }
+        }
+    }
+}
---
+++ Guia_8/EJ1_con metodos/Program.cs
@@ -0,0 +1,150 @@
+﻿using System.ComponentModel;
+
+namespace EJ1
+{
+    internal class Program
+    {
+        #region resolución del problema
+        static int contador, acumulador, maximo, minimo;
+        static int CantIngreso;
+        static bool salir;
+        static void Inicializar()
+        {
+            contador = 0; acumulador = 0; maximo = 0; minimo = 1000;
+            CantIngreso = 0;  
+            salir = false;
+        }
+        static void SolicitarIngresos()
+        {
+            Console.WriteLine("Ingresar cantidad de datos");
+            CantIngreso = Convert.ToInt32(Console.ReadLine());
+        }
+        static void RegistrarValor(int valor)
+        {
+            acumulador += valor;
+            contador++;
+           
+            if (contador==1 ||  valor > maximo)
+                maximo = valor;
+
+            if (contador==1 || valor < minimo)
+                minimo = valor;
+          
+        }
+        static double CalcularPromedio()
+        {
+            double promedio = 0;
+
+            if (contador > 0)
+            {
+                promedio = 1d * acumulador / contador;
+            }
+
+            return promedio;
+        }
+        #endregion
+
+        #region impresion de pantalla
+        static int MostrarPantallSolicitarOpcionMenu()
+        {
+
+            Console.Clear();
+            Console.WriteLine("1 - Procesar un solo numero");
+            Console.WriteLine("2 - Procesar varios numero");
+            Console.WriteLine("3 - Mostrar maximo y minimo");
+            Console.WriteLine("4 - Mostrar Promedio");
+            Console.WriteLine("5 - Mostrar Cantidad de numeros ingresados");
+            Console.WriteLine("6 - Reiniciar Variables\n");
+
+            int entrada = Convert.ToInt32(Console.ReadLine());
+            return entrada;
+        }
+
+        static void MostrarPantallaSolicitarNumero()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ingrese valor");
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+            RegistrarValor(valor);
+
+        }
+
+        static void MostrarPantallaCalcularYMostrarPromedio()
+        {
+            Console.Clear();
+
+            Console.WriteLine(" Cálculo del promedio \n\n");
+
+
+            double promedio=CalcularPromedio();
+            Console.WriteLine($"El promedio es {promedio}");
+
+            Console.WriteLine("Presione una tecla cualquier para continuar");
+            Console.ReadKey();
+
+        }
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Inicializar();
+
+            int op = MostrarPantallSolicitarOpcionMenu();
+
+            while (op != -1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarNumero();
+                        break;
+                    case 2:
+                        {
+                            SolicitarIngresos();
+                            for (int i = 1; i <= CantIngreso; i++)
+                            {
+                                Console.WriteLine("Ingresar Valor");
+                                int valor = Convert.ToInt32(Console.ReadLine());
+                                RegistrarValor(valor);
+                            }
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine($"El valor maximo es {maximo}");
+                            Console.WriteLine($"El valor minimo es {minimo}");
+                        }
+                        break;
+                    case 4:
+                        {
+                            MostrarPantallaCalcularYMostrarPromedio();
+                            break;
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine($"La cantidad de numero ingresados es {contador}");
+                        }
+                        break;
+                    case 6:
+                        {
+                            Inicializar();
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Numero no valido\n");
+                            salir = true;
+                        }
+                        break;
+                }
+
+                if(op!=-1)
+                    op = MostrarPantallSolicitarOpcionMenu();
+            }
+        }
+    }
+}
